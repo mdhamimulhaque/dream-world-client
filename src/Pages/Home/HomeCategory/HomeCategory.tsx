c49b0b4be4d1ed91c59e1d0adf486379bb416238
Md Hamimul Haque <mdhamimulhaque@gmail.com>
2022-12-30 11:01:25 +0600

    home category dynamic added

--- src/Pages/Home/HomeCategory/HomeCategory.tsx
+++ src/Pages/Home/HomeCategory/HomeCategory.tsx
@@ -1,34 +1,48 @@
 import React from 'react';
 import { HiAcademicCap, HiGlobeAsiaAustralia, HiCursorArrowRipple, HiUserGroup } from "react-icons/hi2";
 
+type Categories = {
+    id: number;
+    icon: JSX.Element;
+    name: string;
+}[]
+
+const categories: Categories = [
+    {
+        id: 1,
+        icon: <HiCursorArrowRipple className='text-5xl duration-300 text-green-300' />,
+        name: 'Technology'
+    },
+    {
+        id: 2,
+        icon: <HiAcademicCap className='text-5xl duration-300 text-green-300' />,
+        name: 'Academic'
+    },
+    {
+        id: 3,
+        icon: <HiUserGroup className='text-5xl duration-300 text-green-300' />,
+        name: 'Sports'
+    },
+    {
+        id: 4,
+        icon: <HiGlobeAsiaAustralia className='text-5xl duration-300 text-green-300' />,
+        name: 'Global'
+    }
+]
 
 const HomeCategory: React.FC = () => {
     return (
         <>
-            <div className="home_category_box bg-green-100 shadow-sm hover:shadow hover:text-green-400 duration-300 cursor-pointer p-4 flex justify-center flex-col">
-                <span className='flex justify-center'>
-                    <HiCursorArrowRipple className='text-5xl duration-300 text-green-300' />
-                </span>
-                <h3 className='text-2xl text-center duration-300'>Technology</h3>
-            </div>
-            <div className="home_category_box bg-green-100 shadow-sm hover:shadow hover:text-green-400 duration-300 cursor-pointer p-4 flex justify-center flex-col">
-                <span className='flex justify-center'>
-                    <HiAcademicCap className='text-5xl duration-300 text-green-300' />
-                </span>
-                <h3 className='text-2xl text-center duration-300'>Academic</h3>
-            </div>
-            <div className="home_category_box bg-green-100 shadow-sm hover:shadow hover:text-green-400 duration-300 cursor-pointer p-4 flex justify-center flex-col">
-                <span className='flex justify-center'>
-                    <HiUserGroup className='text-5xl duration-300 text-green-300' />
-                </span>
-                <h3 className='text-2xl text-center duration-300'>Sports</h3>
-            </div>
-            <div className="home_category_box bg-green-100 shadow-sm hover:shadow hover:text-green-400 duration-300 cursor-pointer p-4 flex justify-center flex-col">
-                <span className='flex justify-center'>
-                    <HiGlobeAsiaAustralia className='text-5xl duration-300 text-green-300' />
-                </span>
-                <h3 className='text-2xl text-center duration-300'>Global</h3>
-            </div>
+            {
+                categories.map(categoryItem =>
+                    <div key={categoryItem?.id} className="home_category_box bg-green-100 shadow-sm hover:shadow hover:text-green-400 duration-300 cursor-pointer p-4 flex justify-center flex-col">
+                        <span className='flex justify-center'>
+                            {categoryItem?.icon}
+                        </span>
+                        <h3 className='text-2xl text-center duration-300'>{categoryItem?.name}</h3>
+                    </div>
+                )
+            }
         </>
     );
 };
